55f4fa4151c78ca1706b0d7728ff47c63ea19c3d
ShahbozbekCoder <shahbozbey99@gmail.com>
2024-04-24 16:56:07 +0500

    last update

--- app/src/main/java/com/example/carpicker/MainActivity.kt
+++ app/src/main/java/com/example/carpicker/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CarPicker() {
     CarImageAndButton(
+        
         Modifier
             .fillMaxSize()
             .wrapContentSize(align = Alignment.Center)
